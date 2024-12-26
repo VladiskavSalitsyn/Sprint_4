17c3bf56bd5c5c0c64e2fe195dadcf4ac435e2c7
Владислав Салицын <salitsyn95@mail.ru>
2024-12-26 23:30:39 +0400

    final commit

--- src/test/java/ru/yandex/praktikum/test/FaqAccordionTest.java
+++ src/test/java/ru/yandex/praktikum/test/FaqAccordionTest.java
@@ -24,9 +24,6 @@ public class FaqAccordionTest {
     private final boolean faqAccordionItemPanelHiddenExpected; //true - element is hidden, false - element isn't hidden
     private final String faqAccordionItemPanelTextExpected;
 
-    // Локаторы
-    public static final String ACCORDION_HEADING= "accordion__heading";
-
     public FaqAccordionTest(String accordionItemId, boolean faqAccordionItemPanelHiddenExpected, String faqAccordionItemPanelText){
         this.faqAccordionItemId = accordionItemId;
         this.faqAccordionItemPanelHiddenExpected = faqAccordionItemPanelHiddenExpected;
@@ -36,19 +33,9 @@ public class FaqAccordionTest {
     @Parameterized.Parameters
     public static Object[][] getInputData() {
         return new Object[][] {
-                createTestData(0, "Сутки — 400 рублей. Оплата курьеру — наличными или картой."),
-                createTestData(1, "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."),
-                createTestData(2, "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."),
-                createTestData(3, "Только начиная с завтрашнего дня. Но скоро станем расторопнее."),
-                createTestData(4, "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."),
-                createTestData(5, "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится."),
-                createTestData(6, "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."),
-                createTestData(7, "Да, обязательно. Всем самокатов! И Москве, и Московской области.")
-
+                {"accordion__heading-0", false, "Сутки — 400 рублей. Оплата курьеру — наличными или картой."},
+                {"accordion__heading-3", false, "Только начиная с завтрашнего дня. Но скоро станем расторопнее."},
         };
-    }
-    private static Object[] createTestData(int index, String text) {
-        return new Object[]{ACCORDION_HEADING + "-" + index, false, text};
     }
 
     @Before
